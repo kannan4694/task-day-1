cc1052dab9a5fca34a16dfce7334fc2389db4e89
kannan4694 <kannan.shadow@gmail.com>
2024-11-21 11:47:41 +0530

    day1

---
+++ tests/newproject.spec.js
@@ -0,0 +1,18 @@
+const { test, expect, chromium } = require('@playwright/test');
+
+// test new task, (async ({ }) => 
+//   const browser = await chromium.launch({headless: false });
+//    const page=await browser.newPage();
+//    await page.goto(`www.amazon.com`);
+//    await page.goto(`www.flipkart`);
+//    await page.screenshot(path= easy.png);
+// });
+test  ("new task",async ({ }) => {
+    const browser = await chromium.launch({headless: false });
+    await page.goto('www.deodap.com');
+    const page=await browser.newPage();
+   await page.goto(`www.amazon.com`);
+   await page.goto(`www.flipkart`);
+   await page.screenshot(path= easy.png);
+  });
+    
